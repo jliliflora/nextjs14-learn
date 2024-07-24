ba4010ddb4b0a15cc7da5168446ed2ef923343a4
jliliflora <syon704@gmail.com>
2024-07-25 01:04:20 +0900

    3.2 Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,24 @@
-"use client"
-import { useEffect, useState } from "react";
+export const metadata = {
+    title: "Home"
+}
 
-// export const metadata = {
-//     title: "Home"
-// }
+//server component에서 NEXTJS는 우리가 fetch한 걸 기억해주기 떄문에 한번만 로딩함! 첫번째 fetch만 API에 요청하는 거임
+// 근데 이런경우 헤더부분이나 타이틀부분까지 같이 로딩이 걸려버리는 문제점이 있음띠
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMoives] = useState([]);
-    const getMovies = async () => {
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMoives(json);
-        setIsLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, []);
+async function getMovies() {
+    // console.log("im fetching!");
+    await new Promise((resolve) => setTimeout(resolve, 5000)); //로딩시간 5초
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
     <div>
-        {isLoading ? "Loading..." : JSON.stringify(movies)}
+        {JSON.stringify(movies)}
     </div>
     );
 }
