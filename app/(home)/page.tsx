c41b8e4e5a576eb40e7cfbe8d7ece5e5aea68912
jliliflora <syon704@gmail.com>
2024-07-25 17:38:39 +0900

    3.4 Parallel requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "Home"
 }
 
-// server componenet에서 fetch하는 중에 loading파일을 제공해주면 알아서 로딩파일이 페이지에 나타남
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
     // console.log("im fetching!");
-    await new Promise((resolve) => setTimeout(resolve, 10000)); //로딩시간 10초
-    const response = await fetch(URL);
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
@@ -16,8 +18,12 @@ async function getMovies() {
 export default async function HomePage() {
     const movies = await getMovies();
     return (
-    <div>
-        {JSON.stringify(movies)}
-    </div>
+        <div>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </div>
     );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,29 @@
-export default function MovieDetail({ 
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+   console.log(`Fetching movies: ${Date.now()}`);
+   await new Promise((resolve) => setTimeout(resolve, 2000));
+   const response = await fetch(`${API_URL}/${id}`);
+   return response.json();
+ }
+
+ async function getVideos(id: string) {
+   console.log(`Fetching videos: ${Date.now()}`);
+   await new Promise((resolve) => setTimeout(resolve, 2000));
+   const response = await fetch(`${API_URL}/${id}/videos`);
+   return response.json();
+ }
+
+ //Promise.all을 사용하면, getMovie와 getVideos를 동시에 await하게된다!
+
+export default async function MovieDetail({ 
     params: {id},
  }: {
     params: { id: string }
  }) {
-    //console.log(props); 
-    //http://localhost:3000/movies/123123?region=kr&page=2 ===> 백엔드에서 받아오는 값: { params: { id: '123123' }, searchParams: { region: 'kr', page: '2' } }
-    return <h1>Movie {id}</h1>;
+   console.log("=============")
+   console.log("start fetching");
+   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)])
+   console.log("start fetching");
+    return <h1>{movie.title}</h1>;
 }
