0a1b1ef2a6ca3853f99c94262db957ee82eafbb8
jliliflora <syon704@gmail.com>
2024-07-25 23:24:44 +0900

    3.5 Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,29 +1,47 @@
+import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
-async function getMovie(id: string) {
+/*
+async function getMovieTest(id: string) {
    console.log(`Fetching movies: ${Date.now()}`);
    await new Promise((resolve) => setTimeout(resolve, 2000));
    const response = await fetch(`${API_URL}/${id}`);
    return response.json();
  }
-
+ 
  async function getVideos(id: string) {
    console.log(`Fetching videos: ${Date.now()}`);
    await new Promise((resolve) => setTimeout(resolve, 2000));
    const response = await fetch(`${API_URL}/${id}/videos`);
    return response.json();
- }
+ }*/
 
- //Promise.all을 사용하면, getMovie와 getVideos를 동시에 await하게된다!
+
+ //Promise.all을 사용하지않고 각각 컴포넌트로 정리해주고 render해줌
+ // 여기서 중요한 점dms <Suspense>로 감싸준점!!!
 
 export default async function MovieDetail({ 
     params: {id},
  }: {
     params: { id: string }
  }) {
-   console.log("=============")
+   console.log("===========");
    console.log("start fetching");
-   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)])
-   console.log("start fetching");
-    return <h1>{movie.title}</h1>;
+//   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+//   console.log("end fetching");
+//   return <h1>{movie.title}</h1>;
+   return (
+      <div>
+         <h3>Movie detail page</h3>
+         <Suspense fallback={<h1>Loading movie info</h1>}>
+         <MovieInfo id={id} />
+         </Suspense>
+         <Suspense fallback={<h1>Loading movie videos</h1>}>
+         <MovieVideos id={id} />
+         </Suspense>
+      </div>
+   )
+   
 }
