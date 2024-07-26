d837ff778fbb9deb6b79f0297a537a41c54165a2
jliliflora <syon704@gmail.com>
2024-07-26 15:45:04 +0900

    4.2

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,22 +1,23 @@
-"use client" //usePathname을 사용하려면 추가해줘야함
+"use client"; //usePathname을 사용하려면 추가해줘야함
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styles from "../styles/navigation.module.css"
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
-    const path = usePathname();
-    // console.log(path);
-    return (
-        <nav className={styles.nav}>
-            <ul className={styles.list}>
-                <li>
-                    <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
-                </li>
-                <li>
-                    <Link href="/about-us">About us</Link> {path === "/about-us" ? "🔥" : ""}
-                </li>
-            </ul>
-        </nav>
-    )
+  const path = usePathname();
+  // console.log(path);
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About us</Link>{" "}
+          {path === "/about-us" ? "🔥" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
 }
