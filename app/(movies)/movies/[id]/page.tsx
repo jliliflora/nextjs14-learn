3550cd3460c4e14aedf80ad22966b758240ecf87
jliliflora <syon704@gmail.com>
2024-07-26 16:50:53 +0900

    4.4

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -18,30 +18,28 @@ async function getMovieTest(id: string) {
    return response.json();
  }*/
 
+// Promise.all을 사용하지않고 병렬적으로 불러오는방법임
+// 여기서 중요한 점은 <Suspense>로 감싸준점!!! 각각 컴포넌트로 정리해주고 render해줌
 
- // Promise.all을 사용하지않고 병렬적으로 불러오는방법임
- // 여기서 중요한 점은 <Suspense>로 감싸준점!!! 각각 컴포넌트로 정리해주고 render해줌
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  console.log("===========");
+  console.log("start fetching");
+  //   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  //   console.log("end fetching");
+  //   return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
 
-export default async function MovieDetail({ 
-    params: {id},
- }: {
-    params: { id: string }
- }) {
-   console.log("===========");
-   console.log("start fetching");
-//   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-//   console.log("end fetching");
-//   return <h1>{movie.title}</h1>;
-   return (
-      <div>
-         <h3>Movie detail page</h3>
-         <Suspense fallback={<h1>Loading movie info</h1>}>
-         <MovieInfo id={id} />
-         </Suspense>
-         <Suspense fallback={<h1>Loading movie videos</h1>}>
-         <MovieVideos id={id} />
-         </Suspense>
-      </div>
-   )
-   
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
